4eb70247ad1ca22d4b918ced84124b7f68cb5f5e
avermeulen <andre.vermeulen@gmail.com>
2015-12-02 14:32:51 +0200

    added routes

--- public/app.js
+++ public/app.js
@@ -1,8 +1,23 @@
 var routeApp = angular.module('routeApp', ['ngRoute']);
 
+routeApp.controller('EventController', function($scope){
+    $scope.name = "Ola!";
+});
+
+routeApp.controller('EventDetailsController', function($scope, $routeParams){
+    $scope.name = "Ola Details : " + $routeParams.id;
+});
+
+
 routeApp.config(function($routeProvider){
 
-    $routeProvider.when('/events', {templateUrl:'templates/events.html'});
-    $routeProvider.when('/events/:id', {templateUrl:'templates/event_details.html'});
+    $routeProvider.when('/events', {
+        templateUrl:'templates/events.html',
+        controller : 'EventController'
+    });
+    $routeProvider.when('/events/:id', {
+        templateUrl:'templates/event_details.html',
+        controller : 'EventDetailsController'
+    });
 
 });
